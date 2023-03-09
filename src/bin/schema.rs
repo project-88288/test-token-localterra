7b6aad37451ea892fd26e570ae315f4f59ebf4f4
project-88288 <chaiya.88288@gmail.com>
2023-03-09 15:19:44 +0700

    as minimum commit

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use your_name_here::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
